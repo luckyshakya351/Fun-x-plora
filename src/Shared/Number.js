2359fd6bab1f00fb1b3d65416fbbd70d3294a302
anand <vermaanand278@gmail.com>
2024-07-12 12:37:09 +0530

    ghf

--- src/Shared/Number.js
+++ src/Shared/Number.js
@@ -17,4 +17,12 @@ export const getNumber = [
         id:3,
         number:"THREE"
     },
+    {
+        id:4,
+        number:"Four"
+    },
+    {
+        id:5,
+        number:"Five"
+    },
 ]
